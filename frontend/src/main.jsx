7925d4149d8288d86bf5d7adc245d10ab185a136
santimontironi <santiimontironi@gmail.com>
2025-07-22 20:41:41 -0300

    configurado y agregado el menu de Bootstrap

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './static/css/styles.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
