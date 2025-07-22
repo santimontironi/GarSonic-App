e68f78dd8f8458e4c1ab76334acc21e5f6a60887
santimontironi <santiimontironi@gmail.com>
2025-07-22 20:41:41 -0300

    configurado y agregado el menu de Bootstrap

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,32 @@
+import { Link } from "react-router-dom"
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-lg bg-body-tertiary" id="nav">
+        <div className="container-fluid">
+            <a className="navbar-brand" href="/">GarSonic</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav ms-auto">
+                    <li className="nav-item">
+                        <Link className="nav-link active" aria-current="page" href="/">Inicio</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link active" aria-current="page" href="/garSonicInfo">¿Qué es GarSonic?</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link active" aria-current="page" href="/rols">Roles</Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link className="nav-link active" aria-current="page" href="/loginPage">Ingresar</Link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+  )
+}
+
+export default Header
