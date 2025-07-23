5e6f09885092490ea305e215114686e6f316d093
santimontironi <santiimontironi@gmail.com>
2025-07-23 20:34:22 -0300

    estilos en index

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages"
-import Header from "./components/Header";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header/>
       <Routes>
         <Route path="/" element={<Index/>}/>
       </Routes>
--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -1,7 +1,13 @@
+import Header from "../components/Header";
+
 const Index = () => {
   return (
     <main className="index">
-    
+      <Header/>
+      <div className="contentIndex">
+        <h1 className="tituloIndex">Te damos la bienvenida a GarSonic</h1>
+        <p className="introIndex">Tu nueva forma de escuchar música está acá. Descubrí artistas, explorá géneros y disfrutá de tus canciones favoritas en un solo lugar. GarSonic es una experiencia musical simple, rápida y sin distracciones. ¿Listo para empezar? Dale play y sentí la música.</p>
+      </div>
     </main>
   )
 }
