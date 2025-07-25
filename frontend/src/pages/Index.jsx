58a0d94b041f94ff2849a835e9904f2b79f4a616
santimontironi <santiimontironi@gmail.com>
2025-07-24 23:21:51 -0300

    estilos en index, se agrego el boton de comenzar

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -2,13 +2,19 @@ import Header from "../components/Header";
 
 const Index = () => {
   return (
-    <main className="index">
+    <>
       <Header/>
-      <div className="contentIndex">
-        <h1 className="tituloIndex">Te damos la bienvenida a GarSonic</h1>
-        <p className="introIndex">Tu nueva forma de escuchar música está acá. Descubrí artistas, explorá géneros y disfrutá de tus canciones favoritas en un solo lugar. GarSonic es una experiencia musical simple, rápida y sin distracciones. ¿Listo para empezar? Dale play y sentí la música.</p>
-      </div>
-    </main>
+      <main className="index">
+        <div className="contentIndex">
+          <h1 className="tituloIndex">Te damos la bienvenida a GarSonic</h1>
+          <p className="introIndex">Tu nueva forma de escuchar música está acá. Descubrí artistas, explorá géneros y disfrutá de tus canciones favoritas en un solo lugar. GarSonic es una experiencia musical simple, rápida y sin distracciones. ¿Listo para empezar? Dale play y sentí la música.</p>
+          <div className="btnIndexBox">
+            <a className="btnIndex" href="/loginPage">Comenzar</a>
+          </div>
+          
+        </div>
+      </main>
+    </>
   )
 }
 
