92172c1195b2a4a24616c77378ff8cb503f5ba4f
santimontironi <santiimontironi@gmail.com>
2025-07-28 19:34:49 -0300

    implementacion de la cookie de usuario para manejar la proteccion de rutas

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -3,17 +3,19 @@ import axios from 'axios'
 const URL_BACKEND = 'http://localhost:3000'
 
 export function registerUserAxios(user){
-    return axios.post(`${URL_BACKEND}/register`,user)
+    return axios.post(`${URL_BACKEND}/register`,user,{
+        withCredentials:true
+    })
 }
 
 export function loginUserAxios(user){
-    return axios.post(`${URL_BACKEND}/login`,user)
+    return axios.post(`${URL_BACKEND}/login`,user,{
+        withCredentials:true
+    })
 }
 
 export function dashboardUser(){
     return axios.get(`${URL_BACKEND}/dashboardUser`,{
-        headers: {
-            Authorization: `Bearer ${user?.token}`
-        }
+        withCredentials: true
     })
 }
