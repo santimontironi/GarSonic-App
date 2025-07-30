555291d70dff6e4528c57bab13aa316d7e3aa55c
santimontironi <santiimontironi@gmail.com>
2025-07-29 22:28:03 -0300

    se modifico el registro de usuario desde React, ahora se puede subir la foto de perfil

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,9 +2,10 @@ import axios from 'axios'
 
 const URL_BACKEND = 'http://localhost:3000'
 
-export function registerUserAxios(user){
-    return axios.post(`${URL_BACKEND}/register`,user,{
-        withCredentials:true
+export function registerUserAxios(formData){
+    return axios.post(`${URL_BACKEND}/register`,formData,{
+        withCredentials:true,
+        "Content-Type": "multipart/form-data"
     })
 }
 
