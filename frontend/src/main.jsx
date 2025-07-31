68a2e5e956b1478371d03f68d1d93f21e8929765
santimontironi <santiimontironi@gmail.com>
2025-07-30 21:16:39 -0300

    tailwind incorporado

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,8 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './static/css/global.css'
-import './static/css/stylesPages.css'
 import './static/css/stylesComponents.css'
 import './static/css/hover.css'
 import { AuthProvider } from './context/AuthProvider.jsx'
--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -1,22 +1,20 @@
-import Header from "../components/Header";
 import { motion } from "framer-motion";
 
 const Index = () => {
   return (
     <>
-      <Header/>
-      <main className="index-container">
+      <main className="index-container w-full h-screen bg-gradient-to-r from-purple-900 to-purple-500">
         <motion.div
           initial={{ opacity: 0, y:70 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.7 }}
           viewport={{ once: true }}
         >
-          <div className="contentIndex">
-            <h1 className="tituloIndex">Te damos la bienvenida a GarSonic</h1>
-            <p className="introIndex">Tu nueva forma de escuchar música está acá. Descubrí artistas, explorá géneros y disfrutá de tus canciones favoritas en un solo lugar. GarSonic es una experiencia musical simple, rápida y sin distracciones. ¿Listo para empezar? Dale play y sentí la música.</p>
-            <div className="btnIndexBox">
-              <a className="btnIndex" href="/ingresar">Comenzar</a>
+          <div className="contentIndex text-white flex flex-col gap-3 h-[800px] w-full justify-center items-center">
+            <h1 className="tituloIndex text-center text-[45px] xl:text-[75px]">Te damos la bienvenida a GarSonic</h1>
+            <p className="w-[380px] text-center xl:text-[20px] xl:w-[850px]">Tu nueva forma de escuchar música está acá. Descubrí artistas, explorá géneros y disfrutá de tus canciones favoritas en un solo lugar. GarSonic es una experiencia musical simple, rápida y sin distracciones. ¿Listo para empezar? Dale play y sentí la música.</p>
+            <div className="mt-3">
+              <a className="bg-white p-[10px] text-purple-700 hover:shadow-[4px_5px_6px_rgba(0,0,0,0.50)] rounded-[10px] xl:rounded-[12px] xl:text-[20px]" href="/ingresar">Comenzar</a>
             </div>
           </div>
         </motion.div>
