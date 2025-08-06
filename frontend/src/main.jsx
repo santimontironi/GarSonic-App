387be2d1d8113636fb9a42cc896dea49f87c60ea
santimontironi <santiimontironi@gmail.com>
2025-08-06 18:26:03 -0300

    header y nav hecho con tailwind

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import './static/css/index.css'
+import './static/css/stylesPages.css'
 import './static/css/stylesComponents.css'
 import './static/css/hover.css'
 import { AuthProvider } from './context/AuthProvider.jsx'
