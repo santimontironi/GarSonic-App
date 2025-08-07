a65ed5b5291f57faa3057ce65131a205769b078c
santimontironi <santiimontironi@gmail.com>
2025-08-07 10:15:35 -0300

    solucion en breakpoints en Index y Header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -15,29 +15,29 @@ const Header = () => {
     }
 
 return (
-    <header className="xl:relative lg:relative w-full h-[100px] flex justify-between bg-black items-center lg:pl-[50px] xl:pr-[50px] pl-[20px] pr-[20px] shadow-[10px_25px_25px_rgba(0,0,0,0.50)]">
+    <header className="xl:relative lg:relative w-full h-[100px] lg:h-[150px] flex justify-between bg-black items-center lg:pl-[50px] lg:pr-[50px] pl-[20px] pr-[20px] shadow-[10px_25px_25px_rgba(0,0,0,0.50)]">
             <div>
-                    <a href="/"><img className="w-[100px]" src={logo} alt="logo" /></a>
+                <a href="/"><img className="w-[100px]" src={logo} alt="logo" /></a>
             </div>
 
-            <button className="lg:hidden xl:hidden border-none bg-purple-500 w-[50px] h-[50px] flex justify-center items-center rounded-[10px]" onClick={handleOpenNav}>
-                    <i className="bi bi-list text-white text-[25px]"></i>
+            <button className="lg:hidden border-none bg-purple-500 w-[50px] h-[50px] flex justify-center items-center rounded-[10px]" onClick={handleOpenNav}>
+                <i className="bi bi-list text-white text-[25px]"></i>
             </button>
 
             <nav className={`${openNav ? "opacity-100 translate-x-0" : "opacity-0 translate-x-full"} transition-all duration-300 ease-in-out text-white list-none fixed top-0 right-0 w-[200px] p-[15px] h-[100vh] bg-black
 
-            lg:static lg:flex lg:flex-row lg:gap-[30px] lg:transition-none lg:opacity-100 lg:h-auto lg:w-auto lg:bg-transparent lg:translate-x-0
-            xl:static xl:flex xl:flex-row xl:gap-[30px] xl:bg-transparent xl:transition-none xl:opacity-100 xl:translate-x-0 `}>
+            
+            lg:static lg:flex lg:flex-row lg:gap-[30px] lg:transition-none lg:opacity-100 lg:h-auto lg:w-auto lg:bg-transparent lg:translate-x-0`}>
 
-                    <ul className="flex flex-col justify-center items-center mt-[150px] gap-[20px] xl:flex-row xl:mt-[0px]">
-                            <button className="lg:hidden xl:hidden border-none bg-purple-500 w-[50px] h-[50px] flex justify-center items-center rounded-[10px]" onClick={handleCloseNav}>
-                                    <i className="bi bi-x-lg text-white text-[25px]"></i>
-                            </button>
-                            <li ><Link className="text-[13px] lg:text-[16px] xl:text-[16px] p-[8px] hvr-bounce-to-bottom" to="/">Inicio</Link></li>
-                            <li ><Link className="text-[13px] lg:text-[16px] xl:text-[16px] p-[8px] hvr-bounce-to-bottom" to="/garSonicInfo">¿Qué es GarSonic?</Link></li>
-                            <li ><Link className="text-[13px] lg:text-[16px] xl:text-[16px] p-[8px] hvr-bounce-to-bottom" to="/rols">Roles</Link></li>
-                            <li ><Link className="text-[13px] lg:text-[16px] xl:text-[16px] p-[8px] bg-white text-black rounded-xl" to="/ingresar">Ingresar</Link></li>
-                    </ul>
+                <ul className="flex flex-col justify-center items-center mt-[150px] gap-[20px] lg:flex-row lg:mt-[0px]">
+                        <button className="lg:hidden border-none bg-purple-500 w-[50px] h-[50px] flex justify-center items-center rounded-[10px]" onClick={handleCloseNav}>
+                                <i className="bi bi-x-lg text-white text-[25px]"></i>
+                        </button>
+                        <li ><Link className="text-[13px] lg:text-[16px] p-[8px] hvr-bounce-to-bottom" to="/">Inicio</Link></li>
+                        <li ><Link className="text-[13px] lg:text-[16px] p-[8px] hvr-bounce-to-bottom" to="/garSonicInfo">¿Qué es GarSonic?</Link></li>
+                        <li ><Link className="text-[13px] lg:text-[16px] p-[8px] hvr-bounce-to-bottom" to="/rols">Roles</Link></li>
+                        <li ><Link className="text-[13px] lg:text-[16px] p-[8px] bg-white text-black rounded-xl" to="/ingresar">Ingresar</Link></li>
+                </ul>
 
             </nav>
     </header>
--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -5,18 +5,18 @@ const Index = () => {
   return (
     <>
       <Header/>
-      <main className="index-container w-full h-[calc(100vh-100px)] bg-gradient-to-b from-purple-900 to-purple-700">
+      <main className="index-container w-full h-[calc(100vh-100px)] lg:h-[calc(100vh-150px)] md:h-[calc(100vh-100px)] bg-gradient-to-b from-purple-900 to-purple-700">
         <motion.div
           initial={{ opacity: 0, y:70 }}
           whileInView={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.7 }}
           viewport={{ once: true }}
         >
-          <div className="contentIndex text-white flex flex-col gap-3 h-[700px] w-full justify-center items-center">
+          <div className="contentIndex text-white flex flex-col gap-3 h-[600px] lg:h-[640px] w-full justify-center items-center">
             <h1 className="tituloIndex text-center text-[45px] md:text-[55px] xl:text-[75px]">Te damos la bienvenida a GarSonic</h1>
-            <p className="w-[320px] text-center xl:text-[20px] xl:w-[850px] md:text-[18px] md:w-[600px]">Tu nueva forma de escuchar música está acá. Descubrí artistas, explorá géneros y disfrutá de tus canciones favoritas en un solo lugar. GarSonic es una experiencia musical simple, rápida y sin distracciones. ¿Listo para empezar? Dale play y sentí la música.</p>
+            <p className="w-[320px] text-center lg:text-[20px] lg:w-[850px] md:text-[18px] md:w-[600px]">Tu nueva forma de escuchar música está acá. Descubrí artistas, explorá géneros y disfrutá de tus canciones favoritas en un solo lugar. GarSonic es una experiencia musical simple, rápida y sin distracciones. ¿Listo para empezar? Dale play y sentí la música.</p>
             <div className="mt-3">
-              <a className="bg-white p-[10px] text-purple-700 hover:shadow-[4px_5px_6px_rgba(0,0,0,0.50)] rounded-[10px] xl:rounded-[12px] xl:text-[20px]" href="/ingresar">Comenzar</a>
+              <a className="bg-white p-[10px] text-purple-700 hover:shadow-[4px_5px_6px_rgba(0,0,0,0.50)] rounded-[10px] lg:rounded-[12px] lg:text-[20px]" href="/ingresar">Comenzar</a>
             </div>
           </div>
         </motion.div>
