a784018e1124bdf222b24ec6616e0fc2ae4b044d
santimontironi <santiimontironi@gmail.com>
2025-08-12 19:38:38 -0300

    header responsive agregado

--- frontend/src/pages/DashboardUser.jsx
+++ frontend/src/pages/DashboardUser.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react"
 import { UseAuth } from "../context/useAuth.js"
-import logo from '../static/img/logo.png'
+import HeaderDashboard from "../components/HeaderDashboard.jsx"
+
 
 const DashboardUser = () => {
 
@@ -25,40 +26,7 @@ const DashboardUser = () => {
   return (
     <main className="w-full h-screen bg-[#171717]">
        
-      <header className="lg:flex lg:flex-col lg:text-white lg:gap-[50px] lg:h-screen lg:w-[300px] lg:p-[25px] lg:justify-center lg:items-center lg:bg-gradient-to-b lg:from-purple-900 lg:to-black lg:shadow-[10px_25px_25px_rgba(0,0,0,0.50)] lg:relative">
-
-        <div>
-          <img className="w-[200px]" src={logo} alt="logo app" />
-        </div>
-
-        <div className="lg:flex lg:flex-col lg:items-center">
-          <nav>
-            <ul>
-              <li>
-                <a className="lg:w-[150px] lg:p-[10px] lg:text-purple-500 lg:font-bold lg:rounded-2xl lg:bg-white lg:cursor-pointer lg:hover:bg-purple-700 lg:hover:text-white" href="/mis-listas">
-                  Mis listas
-                </a>
-              </li>
-            </ul>
-          </nav>
-        </div>
-
-        <div className="header-center">
-          <form className="lg:flex lg:flex-col lg:items-center lg:justify-center lg:gap-[15px]" method="post">
-            <input className="lg:w-[200px] lg:p-[10px] lg:text-white lg:border-2 lg:border-white" type="text" placeholder="Ingrese el artista" />
-            <button className="lg:w-[150px] lg:p-[10px] lg:text-white lg:bg-purple-500 lg:cursor-pointer lg:hover:bg-purple-700 lg:rounded-2xl" type="submit">Buscar</button>
-          </form>
-        </div>
-
-        <div className="lg:flex lg:flex-col lg:items-center lg:gap-[10px]">
-          <img className="lg:w-[100px] lg:h-[100px] lg:rounded-full lg:object-cover lg:border-2 lg:border-white" src={`http://localhost:3000/uploads/${userData?.profilePhoto}`}  alt={`foto de perfil de ${userData?.username} `} />
-          <span className="lg:font-bold">{userData?.username}</span>
-          <button className="lg:w-[150px] lg:p-[10px] lg:text-white lg:font-bold lg:rounded-2xl lg:bg-red-500 lg:cursor-pointer lg:hover:bg-purple-700 lg:hover:text-white" onClick={logout}>Cerrar sesión</button>
-        </div>
-
-      </header>
-
-      
+      <HeaderDashboard />
 
       {errorData && <p>{errorData}</p>}
 
