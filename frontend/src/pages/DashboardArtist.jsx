571e2b3746fd179820a7fe2775bbfe9979b677b7
santimontironi <santiimontironi@gmail.com>
2025-08-31 15:15:52 -0300

    renderizacion de la descripcion del artista en su dashboard

--- frontend/src/pages/DashboardArtist.jsx
+++ frontend/src/pages/DashboardArtist.jsx
@@ -33,7 +33,7 @@ const DashboardArtist = () => {
       toast.success(location.state.successMessage)
       navigate(location.pathname, { replace: true })
     }
-  }, [location,navigate])
+  }, [location, navigate])
 
 
   return (
@@ -51,8 +51,11 @@ const DashboardArtist = () => {
         <form className="absolute top-10" method="post">
           <input className="bg-white p-2 w-[370px] md:w-[600px] md:p-3 text-purple-800 rounded-[8px] focus:outline-purple-900" type="text" placeholder="Buscar canciones..." />
         </form>
+
         <h1 className="text-white tituloDashboard text-[40px] border-b-2 border-purple-600 md:text-[60px]">Bienvenido <span>{artistData?.artistName}</span></h1>
+
         <p className="text-white text-[16px] md:text-[19px] w-[300px] md:w-[600px] text-center">Este es tu panel de artista, donde podrás gestionar tu música, crear nuevos lanzamientos y conectar con tus oyentes.</p>
+
         <figure className="w-[150px] md:w-[200px] md:h-[200px] h-[150px] bg-purple-600 rounded-full flex items-center justify-center">
           <img
             className="w-[145px] h-[145px] md:w-[185px] md:h-[185px] rounded-full object-cover"
@@ -60,10 +63,16 @@ const DashboardArtist = () => {
             alt={`foto de perfil de ${artistData?.artistName}`}
           />
         </figure>
+
+        <p className="text-gray-300 italic text-[14px] md:text-[16px] w-[300px] md:w-[600px] text-center">
+          "{artistData.description}"
+        </p>
+
         <div className="flex gap-[15px]">
           <a href="/artista/misCanciones" className="text-white p-[10px] bg-purple-600 rounded-[8px] hover:bg-white hover:text-black">Mis Canciones</a>
           <a href="/artista/subirCancion" className="text-white p-[10px] bg-purple-600 rounded-[8px] hover:bg-white hover:text-black">Agregar canción</a>
         </div>
+
         <button
           className="absolute bottom-5 right-5 w-[150px] p-[10px] text-white font-bold rounded-2xl bg-red-500 cursor-pointer hover:bg-purple-700 hover:text-white"
           onClick={logout}>
