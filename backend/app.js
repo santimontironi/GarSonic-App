749f06df657e4152a6f5a8e545030c7da756a66d
santimontironi <santiimontironi@gmail.com>
2025-09-20 16:40:48 -0300

    cors arreglado

--- backend/app.js
+++ backend/app.js
@@ -12,7 +12,7 @@ const __dirname = path.dirname(__filename)
 const app = express()
 
 app.use(cors({
-    origin:'http://localhost:5173',
+    origin: process.env.FRONTEND_URL,
     credentials:true
 }))
 
