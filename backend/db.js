f2a75a9140a642108051270641beba419c0f00f5
santimontironi <santiimontironi@gmail.com>
2025-11-12 23:36:48 -0300

    solucion en variable de entorno de mongodb

--- backend/db.js
+++ backend/db.js
@@ -3,11 +3,11 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const MONGO_URL = process.env.MONGO_URI;
+const MONGO_URL = process.env.MONGODB_URI;
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log("✅ MongoDB conectado correctamente");
   } catch (error) {
     console.error("❌ Error al conectar a MongoDB:", error.message);
